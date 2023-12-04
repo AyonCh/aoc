3ea08e7a65a09a8eba8fa0d3394f2945d33297b9
AyonC <ayon@github.com>
2023-12-04 20:04:25 +0530

    Aoc 2023: day 3

--- 2023/3.py
+++ 2023/3.py
@@ -1,56 +1,56 @@
 try:
-    data = []
+    data = {}
+    idx = 0
     while True:
-        data.append(input())
+        i = input()
+        for x in range(len(i)):
+            data[(idx, x)] = i[x]
+        idx += 1
 except:
+    prev = ""
+    prev1 = ""
+    yes1 = False
+    yes = False
+    yes2 = False
     ans = []
-    for x in range(len(data)):
-        prev = ""
-        prev1 = ""
-        prev2 = ""
-        prev3 = ""
-        yes = False
-        yes1 = False
-        yes2 = False
-        for y in range(len(data[x])):
-            if data[x][y].isnumeric():
-                prev2 += data[x][y]
-                if yes2:
-                    prev3 += data[x][y]
-                if x+1 < len(data):
-                    if y + 1 < len(data):
-                        if not data[x+1][y+1].isnumeric() and data[x+1][y+1] != ".":
-                            yes = True
-                    if y - 1 > -1:
-                        if not data[x+1][y-1].isnumeric() and data[x+1][y-1] != ".":
-                            yes = True
-                    prev += data[x][y]
+    maxx, maxy = max(data)
+    for (x, y) in data:
+        if data[(x, y)] == ".":
+            if yes:
+                ans.append(int(prev))
+            if yes1:
+                ans.append(int(prev1))
+            if yes2:
+                ans.append(int(prev))
+            prev = ""
+            prev1 = ""
+            yes1 = False
+            yes2 = False
+            yes = False
+        else:
+            if yes1 and data[(x, y)].isnumeric():
+                prev1 += data[(x, y)]
 
-                if x-1 > -1:
-                    if y + 1 < len(data):
-                        if not data[x-1][y+1].isnumeric() and data[x-1][y+1] != ".":
-                            yes1 = True
-                    if y - 1 > -1:
-                        if not data[x-1][y-1].isnumeric() and data[x-1][y-1] != ".":
-                            yes1 = True
-                    prev1 += data[x][y]
-                if y + 1 < len(data):
-                    if not data[x][y+1].isnumeric() and data[x][y+1] != ".":
-                        ans.append(int(prev))
-            else:
-                if yes:
+            if data[(x, y)].isnumeric():
+                prev += data[(x, y)]
+            if y+1 <= maxy:
+                if data[(x, y)].isnumeric() and not data[(x, y+1)].isnumeric() and data[(x, y+1)] != ".":
                     ans.append(int(prev))
-                if yes1:
-                    ans.append(int(prev1))
-                if yes2 and len(prev2) > 0:
-                    ans.append(int(prev2))
+                if not data[(x, y)].isnumeric() and data[(x, y)] != "." and data[(x, y+1)] != ".":
+                    yes1 = True
+                if x+1 <= maxx and data[(x, y)].isnumeric():
+                    if y-1 >= 0 and not data[(x+1, y-1)].isnumeric() and data[(x+1, y-1)] != ".":
+                        yes = True
+                    if not data[(x+1, y+1)].isnumeric() and data[(x+1, y+1)] != ".":
+                        yes = True
+                    if not data[(x+1, y)].isnumeric() and data[(x+1, y)] != ".":
+                        yes = True
 
-                yes = False
-                yes1 = False
-                yes2 = False
-                prev = ""
-                prev1 = ""
-                prev2 = ""
-                if not data[x][y].isnumeric() and data[x][y] != ".":
-                    yes2 = True
-    print(f"\nPart 1: {sum(set(ans))}")
+                if x-1 >= 0 and data[(x, y)].isnumeric():
+                    if y-1 >= 0 and not data[(x-1, y-1)].isnumeric() and data[(x-1, y-1)] != ".":
+                        yes2 = True
+                    if not data[(x-1, y+1)].isnumeric() and data[(x-1, y+1)] != ".":
+                        yes2 = True
+                    if not data[(x-1, y)].isnumeric() and data[(x-1, y)] != ".":
+                        yes2 = True
+    print(f"\nPart 1: {sum(ans)}")

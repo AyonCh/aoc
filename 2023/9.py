d92871326c15e78554240e69f0e2c63c866fe906
Replit user <>
2023-12-09 08:04:15 +0000

    Aoc 2023: day 9

--- 2023/9.py
+++ 2023/9.py
@@ -1,8 +1,9 @@
 try:
-  ans = 0
+  ans, ans1 = 0, 0
   while True:
-    values = [list(map(int, input().split(" ")))]
-    change = 0
+    i = list(map(int, input().split(" ")))]
+    values, values1 = [i],[i]
+    change, change1 = 0, 0
     while True:
       temp = []
       for x in range(len(values[-1]) - 1):
@@ -12,6 +13,17 @@ try:
         break
     for x in values[::-1]:
       change += x[-1]
+    while True:
+      temp = []
+      for x in range(len(values1[-1][::-1]) - 1):
+        temp.append(values1[-1][::-1][x+1] - values1[-1][::-1][x])
+      values1.append(temp)
+      if len(values1[-1]) == values1[-1].count(0):
+        break
+    for x in values1[::-1]:
+      change1 += x[-1]
     ans += change
+    ans1 += change1
 except:
   print(f"\nPart 1: {ans}")
+  print(f"Part 2: {ans1}")

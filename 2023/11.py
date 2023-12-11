d49d4e46c33fd17849eb9047f22922b525c13e6e
AyonC <ayon@github.com>
2023-12-11 20:27:57 +0530

    Aoc 2023: day 11

--- 2023/11.py
+++ 2023/11.py
@@ -8,16 +8,17 @@ try:
         for x in range(len(i)):
             a.append(i[x])
             if i[x] == "#":
-                data.append([x,y])
+                data.append([x, y])
         dat.append(a)
         y += 1
 except:
     ans = 0
+    ans1 = 0
     row = []
     col = []
-    cols = list(map(list,zip(*dat)))
-    for x in range(len(data)):
-        if data[x].count(".") == len(data[x]):
+    cols = list(map(list, zip(*dat)))
+    for x in range(len(dat)):
+        if dat[x].count(".") == len(dat[x]):
             row.append(x)
     for x in range(len(cols)):
         if cols[x].count(".") == len(cols[x]):
@@ -25,10 +26,15 @@ except:
     for x in range(len(data)):
         for y in data[x+1:]:
             ans += abs(data[x][0] - y[0]) + abs(data[x][1] - y[1])
+            ans1 += abs(data[x][0] - y[0]) + abs(data[x][1] - y[1])
             for z in col:
                 if max(data[x][0], y[0]) > z and min(data[x][0], y[0]) < z:
                     ans += 1
+                    ans1 += 999999
             for z in row:
                 if max(data[x][1], y[1]) > z and min(data[x][1], y[1]) < z:
                     ans += 1
-    print(ans)
+                    ans1 += 999999
+
+    print(f"\nPart 1: {ans}")
+    print(f"Part 2: {ans1}")

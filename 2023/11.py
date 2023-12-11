d5b9270aba88f90fe44415017cc6ab7dde954ccf
Replit user <>
2023-12-11 10:11:51 +0000

    Aoc 2023: day 11

--- 2023/11.py
+++ 2023/11.py
@@ -9,7 +9,7 @@ try:
         y += 1
 except:
     ans = 0
-    for x in data:
-        for y in data:
-            ans += abs(x[0] - y[0]) + abs(x[1] - y[1]))
+    for x in range(len(data)):
+        for y in data[x+1:]:
+            ans += abs(data[x][0] - y[0]) + abs(data[x][1] - y[1]))
     print(ans)

7f4f770310c77f635f0cdd62c4257f28b74249b3
Replit user <>
2023-12-11 10:42:34 +0000

    Aoc 2023: day 11

--- 2023/11.py
+++ 2023/11.py
@@ -13,7 +13,22 @@ try:
         y += 1
 except:
     ans = 0
+    row = []
+    col = []
+    cols = list(map(list,zip(*dat)))
+    for x in range(len(data)):
+        if data[x].count(".") == len(data[x]):
+            row.append(x)
+    for x in range(len(cols)):
+        if cols[x].count(".") == len(cols[x]):
+            col.append(x)
     for x in range(len(data)):
         for y in data[x+1:]:
-            ans += abs(data[x][0] - y[0]) + abs(data[x][1] - y[1]))
+            ans += abs(data[x][0] - y[0]) + abs(data[x][1] - y[1])
+            for z in col:
+                if max(data[x][0], y[0]) > z and min(data[x][0], y[0]) < z:
+                    ans += 1
+            for z in row:
+                if max(data[x][1], y[1]) > z and min(data[x][1], y[1]) < z:
+                    ans += 1
     print(ans)
